1fd203f106ff6c78acfe41430133d92b5861dce1
Varun Muriyanat <varunmuriyanat@gmail.com>
2021-09-18 07:44:20 -0400

    Add files via upload

--- read_properties.py
+++ read_properties.py
@@ -1,5 +1,5 @@
-with open("property_search4.md", "w") as fw:
-    with open("property_search4.txt", "r") as fp:
+with open("property_search5.md", "w") as fw:
+    with open("property_search5.txt", "r") as fp:
         line = fp.readline()
         cnt = 1
         address = None
@@ -12,13 +12,15 @@ with open("property_search4.md", "w") as fw:
         flrArea = None
         daysOnMarket = None
         residential = None
+        status_change = None
 
+        fw.write("|Status".ljust(16))
         fw.write("|Price".ljust(9))
         fw.write("|B".ljust(1))
         fw.write("|Bat".ljust(3))
         fw.write("|Postal".ljust(8))
         fw.write("|FlrA".ljust(9))
-        fw.write("|Address".ljust(36))
+        fw.write("|Address".ljust(50))
         fw.write("|Day".ljust(4))
         fw.write("|Residential".ljust(61))
         fw.write("|mls".ljust(9))
@@ -27,57 +29,47 @@ with open("property_search4.md", "w") as fw:
         fw.write("\n")
 
         while line:
-            if(cnt > 10):
-                cnt = cnt - 10
+            if(cnt > 11):
+                cnt = cnt - 11
 
-                # include only postal codes starting with T6
-                if(postal.startswith("T6")):
-                    fw.write("|")
-                    fw.write(price + "|")
-                    fw.write(beds  + "|")
-                    fw.write(baths  + "|")
-                    fw.write(postal  + "|")
-                    fw.write(flrArea  + "|")
-                    fw.write(address  + "|")
-                    fw.write(daysOnMarket  + "|")
-                    fw.write(residential  + "|")
-                    fw.write(mls  + "|")
-                    fw.write(description  + "|")
-                    fw.write("\n")
+                fw.write("|")
+                fw.write(status_change + "|")
+                fw.write(price + "|")
+                fw.write(beds  + "|")
+                fw.write(baths  + "|")
+                fw.write(postal  + "|")
+                fw.write(flrArea  + "|")
+                fw.write(address  + "|")
+                fw.write(daysOnMarket  + "|")
+                fw.write(residential  + "|")
+                fw.write(mls  + "|")
+                fw.write(description  + "|")
+                fw.write("\n")
 
             if(cnt == 1):
                 address = line.strip()     
-                address = address.replace(", Edmonton, AB", "")
-                address = address.ljust(35)
+                address = address.ljust(49)
             elif(cnt == 2):
-                postal= line.strip() 
-                postal = postal.replace("Postal Code: ", "")
+                postal= line.strip().replace("Postal Code: ", "")
             elif(cnt == 3):
                 mls = line.strip()
             elif(cnt == 4):
-                residential = line.strip()
-                residential = residential.ljust(60)
+                residential = line.strip().ljust(60)
             elif(cnt == 5):
-                price = line.strip()
-                price = price.replace("Price: ", "")
+                price = line.strip().replace("Price: ", "")
             elif(cnt == 6):
-                description = line.strip()
-                description = description.ljust(1020)
+                description = line.strip().ljust(1020)
             elif(cnt == 7):
-                beds = line.strip()
-                beds = beds.replace("BEDS: ", "")
+                beds = line.strip().replace("BEDS: ", "")
             elif(cnt == 8):
-                baths = line.strip()
-                baths = baths.replace("BATHS: ", "")
+                baths = line.strip().replace("BATHS: ", "")
             elif(cnt == 9):
-                flrArea = line.strip()
-                flrArea = flrArea.replace("FlrArea SF : ", "").strip()
+                flrArea = line.strip().replace("FlrArea SF : ", "").strip().ljust(8)
             elif(cnt == 10):
-                daysOnMarket = line.strip()
-                daysOnMarket = daysOnMarket.replace("Days on Market: ", "").strip()
-                daysOnMarket = daysOnMarket.ljust(3)
+                daysOnMarket = line.strip().replace("Days on Market: ", "").strip().ljust(3)
+            elif(cnt == 11):
+                status_change = line.strip().ljust(15)
 
-            line = fp.readline()
-
+            line = fp.readline() 
             cnt += 1
 
